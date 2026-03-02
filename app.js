93e878fe9c4bb279bdcf19a7c09d238019bc92ad
andy123-glitch <andy.azerot@gmail.com>
2026-03-02 23:16:42 +0100

    add try catch

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
 import express from "express";
 import mongoose from "mongoose";
 
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-
 import userRoutes from "./routes/user.js";
 import bookRoutes from "./routes/book.js";
 
+// Permet d'avoir le chemin jusqu'a ce dossier
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Now you can use __dirname normally
-console.log(__dirname);
-
 const app = express();
+try {
+    const mongodb = await mongoose.connect("mongodb://root:example@localhost:27017/LivreAPI?authSource=admin", {
+        serverSelectionTimeoutMS: 5000,
+    });
+    console.log("Connexion a la base de donnée reussi");
 
-mongoose
-    .connect("mongodb://root:example@localhost:27017/LivreAPI?authSource=admin")
-    .then(() => console.log("connexion a mongoDB reussi !"))
-    .catch((e) => console.log("Erreur de connexion a mongodb : " + e));
+    app.use(express.json());
 
-app.use(express.json());
+    app.use((req, res, next) => {
+        if (req.method !== "OPTIONS") console.log(`Requete ${req.method} : ${req.url} / ${req.body}`);
+        next();
+    });
+    app.use((req, res, next) => {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+        next();
+    });
 
-app.use((req, res, next) => {
-    if (req.method !== "OPTIONS") console.log(`Requete ${req.method} : ${req.url} / ${req.body}`);
-    next();
-});
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    next();
-});
+    app.use("/images", express.static(join(__dirname, "images")));
 
-app.use("/images", express.static(join(__dirname, "images")));
-
-app.use("/api/auth", userRoutes);
-app.use("/api/books", bookRoutes);
+    app.use("/api/auth", userRoutes);
+    app.use("/api/books", bookRoutes);
+} catch (error) {
+    console.log(error);
+}
 
 export default app;
