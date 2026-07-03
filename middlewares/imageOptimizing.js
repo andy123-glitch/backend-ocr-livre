c2ff5a1fd96ae050dc5413261a83e7b6184c9d6a
Andy <121857944+andy123-glitch@users.noreply.github.com>
2026-07-03 14:25:24 +0200

    Comment image optimization middleware

--- middlewares/imageOptimizing.js
+++ middlewares/imageOptimizing.js
@@ -1,21 +1,34 @@
 import sharp from "sharp";
 import fs from "fs";
 
+// Middleware pour optimiser (compresser) une image uploadée
 export const opt = async (req, res, next) => {
+
+    // Vérifie si un fichier a bien été envoyé dans la requête
     if (req.file !== undefined) {
+
+        // Récupère le nom du fichier sans son extension
         const nameFile = req.file.filename.split(".");
-        nameFile.pop();
+        nameFile.pop(); // supprime l'extension
 
+        // Utilise sharp pour redimensionner et convertir l'image en format webp
         await sharp(req.file.path)
-            .resize(800)
-            .webp()
-            .toFile(req.file.destination + "\\\\Compresed_" + nameFile + ".webp", (err, info) => {
-                if (err) throw err;
-                //Supprime les images non compresse
-                fs.unlinkSync(req.file.path);
-            });
+            .resize(800) // redimensionne l'image à une largeur de 800px (hauteur auto)
+            .webp() // convertit l'image en format webp (plus léger)
+            .toFile(
+                req.file.destination + "\\\\Compresed_" + nameFile + ".webp",
+                (err, info) => {
+                    if (err) throw err;
 
+                    // Supprime l'image originale non compressée
+                    fs.unlinkSync(req.file.path);
+                }
+            );
+
+        // Met à jour le nom du fichier dans la requête avec le nouveau fichier compressé
         req.file.filename = "Compresed_" + nameFile + ".webp";
     }
+
+    // Passe au middleware suivant
     next();
 };
